6fef026e5d48909ebbdfbdc95e387b307dd0b91f
Brandon Griffiths <brandonjosephgriffiths@gmail.com>
2022-08-10 21:41:33 -0600

    added orders

--- index.js
+++ index.js
@@ -43,11 +43,15 @@ app.post("/", (req,res) =>{
         questions: req.body.questions,
         notes: req.body.notes,
     }).then(sugar_bird =>{
-        res.redirect("/orders");
+        res.redirect("/thanks");
     }).catch(err => {
         console.log(err);
         res.status(500).json({err});
     });
+});
+
+app.get("/thanks", (req,res) => {
+    res.render("thanks");
 });
 
 app.get("/orders", (req, res) => {
@@ -56,6 +60,35 @@ app.get("/orders", (req, res) => {
     }).catch(err => {
         console.log(err);
         res.status(500).json({err});
+    });
+});
+
+app.get("/view/:id", (req, res) => {
+    knex("sugar_bird").where("order_id", parseInt(req.params.id)).then(new_order => {
+        res.render("view", {sugar_bird : new_order});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+
+app.post("/view", (req,res) =>{
+    knex("sugar_bird").where("order_id", parseInt(req.body.order_id)).update({
+        // pickup_date: req.body.pickup_date,
+        // first_name: req.body.first_name.toUpperCase(),
+        // last_name: req.body.last_name.toUpperCase(),
+        // email: req.body.email,
+        // phone_number: req.body.phone_number,
+        // num_cookies: req.body.num_cookies,
+        // event: req.body.event,
+        // theme: req.body.theme,
+        // colors: req.body.colors,
+        // personalization: req.body.personalization,
+        // questions: req.body.questions,
+        notes: req.body.notes,
+    }).then(sugar_bird =>{
+        res.redirect("/orders");
     });
 });
 
